495b254b473fdfa81401ccc3d9d0f63bb9616e46
indigodarkwolf <indigo.darkwolf@gmail.com>
2019-10-05 20:44:09 -0500

    Cleanup

--- build.bat
+++ build.bat
@@ -3,7 +3,8 @@
 set PROJECT=x16-racer
 set EMU_DIR=..\..\vs2019\x16-bin
 set ASM=bin\cc65\bin\cl65.exe
+set DEBUG_OPTS=-vm -m %PROJECT%.map -g -l %PROJECT%.list -Wl --dbgfile,%PROJECT%.dbg
 
-%ASM% -o %PROJECT%.prg -DC64 %PROJECT%.asm -vm -m %PROJECT%.map -g -l %PROJECT%.list -Wl --dbgfile,%PROJECT%.dbg
+%ASM% -o %PROJECT%.prg -DC64 %PROJECT%.asm
 
 copy %PROJECT%.prg %EMU_DIR%
--- vera.asm
+++ vera.asm
@@ -1,6 +1,7 @@
 .ifndef VERA_ASM
 VERA_ASM=1
 
+.ifdef USING_VERA_STREAM_OUT_RLE
 ;==============================================
 ; vera_stream_out_data
 ; Stream out a block of memory to VERA_data
@@ -44,5 +45,85 @@ vera_stream_out_data:
     dex
     bne @loop2
     rts
+.endif ; USING_VERA_STREAM_OUT_DATA
+
+.ifdef USING_VERA_STREAM_OUT_RLE
+;==============================================
+; vera_stream_out_rle
+; Stream out a block of rle-compressed memory to VERA_data
+;----------------------------------------------
+; INPUT: X   - number of rle pages to stream
+;        Y   - number of rle bytes to stream
+;        $FB - low byte of starting address
+;        $FC - high byte of starting address
+;----------------------------------------------
+; Modifies: A, X, Y, $FC
+;
+vera_stream_out_rle:
+    tya
+    pha
+    ; If no pages to copy, skip to bytes
+    txa
+    cmp #0
+    beq @no_pages
+
+    ; Copy X pages to VERA_data
+    ldy #0
+@page_loop:
+    pha
+
+@tuple_loop:
+    ; First byte is the number of repetitions
+    lda ($FB),Y
+    tax
+
+    iny
+
+    ; Second byte is the value to stream
+    lda ($FB),Y
+    iny
+
+@byte_loop:
+    sta VERA_data
+    dex
+    bne @byte_loop
+
+    cpy #0
+    bne @tuple_loop
+
+    inc $FC
+    pla
+    clc
+    adc #$FF
+    bne @page_loop
+
+@no_pages:
+    ; Copy X bytes to VERA_data
+    ldy #0
+
+@loop2:
+    ; First byte is the number of repetitions
+    lda ($FB),Y
+    tax
+    iny
+
+    ; Second byte is the value to stream
+    lda ($FB),Y
+    iny
+@byte_loop2:
+    sta VERA_data
+    dex
+    bne @byte_loop2
+
+    pla
+    clc 
+    adc #$FE
+    pha
+    bne @loop2
+    pla
+    
+    rts
+
+.endif ; USING_VERA_STREAM_OUT_RLE
 
 .endif ; VERA_ASM
