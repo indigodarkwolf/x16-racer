06d4179ff83a2b74b21973e4f3da2b3be4dbadfa
indigodarkwolf <indigo.darkwolf@gmail.com>
2020-06-26 19:05:24 -0500

    File re-org

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@ set EMU_DIR=..\..\vs2019\x16-bin
 ::set EMU=.\x16emu_Release.exe
 set EMU=.\x16emu_Release.exe
 
-%EMU_DIR%\%EMU% -prg "x16-racer.prg" -debug -scale 2 -quality nearest -joy1 SNES
+%EMU_DIR%\%EMU% -prg "x16-racer.prg" -debug -scale 2 -quality nearest
