04d65ad2bb36bcf09a2f4f9dd86f4bf998003888
indigodarkwolf <indigo.darkwolf@gmail.com>
2023-06-13 17:24:35 -0500

    Fix linkedlist for-macros

--- src/linkedlist.asm
+++ src/linkedlist.asm
@@ -328,43 +328,43 @@
 .endmacro
 
 ;#define LIST_FOR_EACH(NAME, INDEX_NAME) for(INDEX_NAME=.ident (.concat ("list_", NAME, "_used_head")); INDEX_NAME != -1; INDEX_NAME = (.ident (.concat ("list_", NAME, "_next"))[INDEX_NAME] != .ident (.concat ("list_", NAME, "_used_head")) ? .ident (.concat ("list_", NAME, "_next"))[INDEX_NAME] : -1))
-.macro LIST_FOR_BEGIN NAME, FOR_LABEL
+.macro LIST_FOR_BEGIN NAME, FOR_LABEL, PUSH_METHOD
 .ident (.concat (FOR_LABEL, "_begin")):
 	LIST_GET_FIRST NAME
 	cmp #$FF
 	beq .ident (.concat (FOR_LABEL, "_end"))
 .ident (FOR_LABEL):
-	pha
+	PUSH_METHOD
 .endmacro
 
-.macro LIST_FOR_CONTINUE NAME, FOR_LABEL
-	pla
+.macro LIST_FOR_CONTINUE NAME, FOR_LABEL, PULL_METHOD
+	PULL_METHOD
 	LIST_GET_NEXT NAME
 	cmp .ident (.concat ("list_", NAME, "_used_head"))
 	bne .ident (FOR_LABEL)
 	bra SKIP_LABEL
 .endmacro
 
-.macro LIST_FOR_END NAME, FOR_LABEL
-	pla
+.macro LIST_FOR_END NAME, FOR_LABEL, PULL_METHOD
+	PULL_METHOD
 	LIST_GET_NEXT NAME
 	cmp .ident (.concat ("list_", NAME, "_used_head"))
 	bne .ident (FOR_LABEL)
 .ident (.concat (FOR_LABEL, "_end")):
 .endmacro
 
-.macro LIST_LONGFOR_BEGIN NAME, FOR_LABEL
+.macro LIST_LONGFOR_BEGIN NAME, FOR_LABEL, PUSH_METHOD
 .ident (.concat (FOR_LABEL, "_begin")):
 	LIST_GET_FIRST NAME
 	cmp #$FF
 	bne .ident (FOR_LABEL)
 	jmp .ident (.concat (FOR_LABEL, "_end"))
 .ident (FOR_LABEL):
-	pha
+	PUSH_METHOD
 .endmacro
 
-.macro LIST_LONGFOR_CONTINUE NAME, FOR_LABEL
-	pla
+.macro LIST_LONGFOR_CONTINUE NAME, FOR_LABEL, PULL_METHOD
+	PULL_METHOD
 	LIST_GET_NEXT NAME
 	cmp .ident (.concat ("list_", NAME, "_used_head"))
 	.local jump_to_end
@@ -374,8 +374,8 @@ jump_to_end:
 	jmp .ident (.concat (FOR_LABEL, "_end"))
 .endmacro
 
-.macro LIST_LONGFOR_END NAME, FOR_LABEL
-	pla
+.macro LIST_LONGFOR_END NAME, FOR_LABEL, PULL_METHOD
+	PULL_METHOD
 	LIST_GET_NEXT NAME
 	cmp .ident (.concat ("list_", NAME, "_used_head"))
 	beq .ident (.concat (FOR_LABEL, "_end"))
@@ -390,7 +390,10 @@ jump_to_end:
 ;
 ;-------------------------------------------------
 
+.data
 LIST_INSTANTIATE "foo", 100
+
+.code
 LIST_IMPLEMENT "foo", 100
 
 .proc list_test
@@ -402,6 +405,43 @@ LIST_IMPLEMENT "foo", 100
 	LIST_ALLOC "foo"	; Allocates index 4
 	lda #2
 	LIST_FREE "foo"		; Frees index 2
+
+	ldx #2
+	phx
+	LIST_FOR_BEGIN "foo", "test_for", sta $02
+		plx
+		sta $03,x
+		inx
+		phx
+	LIST_FOR_END "foo", "test_for", lda $02
+	plx
+
+	lda #0
+	LIST_FREE "foo"
+	lda #1
+	LIST_FREE "foo"
+	lda #3
+	LIST_FREE "foo"
+	lda #4
+	LIST_FREE "foo"
+
+	LIST_ALLOC "foo"	; Allocates index 5
+	LIST_ALLOC "foo"	; Allocates index 6
+	LIST_ALLOC "foo"	; Allocates index 7
+	LIST_ALLOC "foo"	; Allocates index 8
+	LIST_ALLOC "foo"	; Allocates index 9
+
+	ldx #2
+	phx
+	LIST_LONGFOR_BEGIN "foo", "test_longfor", sta $02
+		plx
+		sta $03,x
+		inx
+		phx
+	LIST_LONGFOR_END "foo", "test_longfor", lda $02
+	plx
+
+	rts
 .endproc
 .export list_test
 
